5add391e18162696714ce962e8493a2349dc37d4
AmrishSS <118153742+AmrishSS@users.noreply.github.com>
2023-07-12 09:05:41 +0530

    Add files via upload

---
+++ day 3/Is Prime.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+// Read only region start
+class UserMainCode {
+    public int isPrime(int input1) {
+        // Read only region end
+        // write code here...
+        if (input1 < 2)
+            return 1;
+        
+        for (int i = 2; i <= Math.sqrt(input1); i++) {
+            if (input1 % i == 0)
+                return 1;
+        }
+        return 2;
+    }
+}
